08351e9a5c6fa90cfd1a06ec85739c11c5d8c33e
Chen Yufei <cyfdecyf@gmail.com>
2022-05-22 22:43:30 +0800

    Add debug.h

---
+++ debug.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstring>
+#include <chrono>
+#include <iostream>
+
+/**
+ * @brief A global variable to control whether or not debug logging code should be enabled.
+ * @detail Debug printing may prevent some races from happen. Use with caution.
+ */
+static constexpr bool debug = false;
+
+static inline uint64_t get_timestamp() {
+    return std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::system_clock::now().time_since_epoch()).count();
+}
+
+#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+
+// msg should be a single string.
+// This is to avoid multiple << resulting interleaved debug message.
+#define DBG(msg) \
+    do { \
+        if constexpr (debug) { \
+            std::cout << std::to_string(get_timestamp()) + " " + (std::string(__FILENAME__)  + ":" + std::to_string(__LINE__) \
+            + " " + (msg) + "\n"); \
+        } \
+    } while (0)
